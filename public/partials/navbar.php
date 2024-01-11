20a7ec8fa0e42df8dab0dfff79c3b00591bbeb21
Kraine07 <aundre.bushay@gmail.com>
2024-01-11 00:56:34 -0500

    progress backup

--- public/partials/navbar.php
+++ public/partials/navbar.php
@@ -1,6 +1,6 @@
 <nav class='flex justify-between items-center bg-gray-900 h-[10%] w-full px-8  sticky  top-0  z-30'>
     <a class='text-2xl font-light text-white' href="main.php">Backyard Cinema Ltd</a>
     <div>
-        <a class='bg-white px-6 py-1 ml-8 rounded-md hover:bg-slate-300 border border-white' href="view-schedule.php">View Schedule</a>
+        <a class='bg-white px-6 py-1 ml-8 rounded-md hover:bg-slate-300 border border-white' href="view-schedule.php">Schedule</a>
     </div>
 </nav>
--- public/view-schedule.php
+++ public/view-schedule.php
@@ -1,4 +1,5 @@
 <?php
+require_once('dbConn.php');
 require_once('./partials/head.php');
 echo "<div class='h-full w-full bg-blue-950'>";
 require_once('./partials/navbar.php');
@@ -9,6 +10,15 @@ $month_day = 1;  // day number to be displayed in calendar
 $current_month = date('F'); // full text representation of a month
 $days_in_month = date('t',strtotime($current_month)); // number of days in current month
 $start_day =  date('w', strtotime( $current_month . ' 01,' . date('Y'))); // number representing the day that starts the month
+
+// date selected by user (format  YYYY-MM-DD)
+$selected_date = date('Y-m-d');
+
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+$selected_date = date('Y-m-'.$_POST['calendar-day']);
+}
+
+$schedule_sql = "SELECT `movie_poster`, `movie_id`, `movie_title`, `movie_plot`, `movie_duration`,`movie_trailer`, `start`, `end`, `movie`, `screen` FROM `{$database}`.`{$schedule_table}`, `{$database}`.`{$movie_table}`, `{$database}`.`{$screen_table}` WHERE `movie` = `movie_id` AND  `screen` = `screen_id` AND FROM_UNIXTIME(`start`,'%Y-%m-%d') = '$selected_date' ORDER BY `start`";
 
 
 ?>
@@ -33,14 +43,14 @@ $start_day =  date('w', strtotime( $current_month . ' 01,' . date('Y'))); // num
 
                 // display calendar days
                 for($i=1; $i<=$days_in_month+$start_day; $i++){
-                    $bg = ( $month_day == (int)date('j')) ? "bg-white text-gray-900" :"bg-gray-700";
+                    $border = ( $month_day == (int)date('j')) ? "border border-white" :"";
                     if($i <= $start_day){
                         echo "<span></span>";
                     }else{
                         echo '
-                        <form action="" method="post">
-                            <input type="text" name="calendar-day" value="'.$month_day.'" hidden>
-                            <button class="text-xs aspect-square text-light w-8 p-0 '. $bg .'  rounded-full">'.$month_day.'</button>
+                        <form action="view-schedule.php" method="post">
+                            <input type="text" name="calendar-day" value="'.sprintf("%02d",$month_day).'" hidden>
+                            <button class="text-xs aspect-square text-light w-8 p-0 '. $border .'  rounded-full">'.$month_day.'</button>
                         </form>
                     ';
                     $month_day ++;
@@ -57,7 +67,17 @@ $start_day =  date('w', strtotime( $current_month . ' 01,' . date('Y'))); // num
 
     <!-- schedule views -->
     <div class="w-[840px] h-full bg-slate-700 right-4 absolute overflow-y-auto">
+        <?php
+        $new_date = date_create($selected_date);
+        $new_selected_date = date_format($new_date, 'F j, Y');
+        echo "<span class='text-xl font-bold block'> {$new_selected_date} </span>";
+        if($result = mysqli_query($conn, $schedule_sql)){
+            while($row = mysqli_fetch_assoc($result)){
+                echo $row['movie_title']."<br>";
+            }
 
+        }
+        ?>
     </div>
 
 </div>
