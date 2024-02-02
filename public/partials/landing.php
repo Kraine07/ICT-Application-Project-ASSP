65b3cc6aab138cf1d2516f3061d14b71044b6bf2
Kraine07 <aundre.bushay@gmail.com>
2024-02-01 23:34:38 -0500

    minor fixes

--- public/partials/landing.php
+++ public/partials/landing.php
@@ -19,7 +19,7 @@
         <div class="absolute w-[740px] md:w-[800px] lg:w-11/12 aspect-square rounded-full bg-app-blue -right-1/2 sm:-right-1/4 md:-right-1/4 self-center border-gray-200 border-2 "></div>
 
         <div class=" w-3/5  flex flex-col justify-evenly items-end z-10">
-            <button id="login" class=" bg-app-tertiary text-white text-xs py-1 px-2 rounded-l-full justify-self-start">
+            <button id="login" class=" bg-app-secondary text-white text-xs py-1 px-2 rounded-l-full justify-self-start">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.0" stroke="currentColor" class="w-4 h-4 inline mx-2">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 5.25a3 3 0 0 1 3 3m3 0a6 6 0 0 1-7.029 5.912c-.563-.097-1.159.026-1.563.43L10.5 17.25H8.25v2.25H6v2.25H2.25v-2.818c0-.597.237-1.17.659-1.591l6.499-6.499c.404-.404.527-1 .43-1.563A6 6 0 1 1 21.75 8.25Z" />
                 </svg>
@@ -28,7 +28,7 @@
             <div class=" w-[200px] md:w-3/4 flex flex-col items-end mr-12 justify-between">
                 <!-- <h1 class="text-8xl text-gray-300 font-light text-right w-3/5">Backyard Cinema</h1> -->
                 <img src="./img/logo_light_2.png" alt="logo" class="w-full object-contain">
-                <a href="main.php" class="bg-app-orange text-app-blue text-center text-xl font-semibold py-1 w-full rounded-full my-8  ">Enter
+                <a href="main.php" class="bg-app-orange text-gray-200 text-center text-xl font-semibold py-1 w-full rounded-full my-8  ">Enter
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 inline animate-push">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M11.25 4.5l7.5 7.5-7.5 7.5m-6-15l7.5 7.5-7.5 7.5" />
                     </svg>
--- public/partials/login-form-modal.php
+++ public/partials/login-form-modal.php
@@ -1,14 +1,14 @@
 
 
 <div id="login-form" class="bg-app-modal absolute top-0 left-30 z-50 hidden w-full h-screen">
-    <div id="" class=" bg-app-secondary  w-[300px] p-8 top-12 right-12 absolute z-50">
+    <div id="" class=" bg-app-tertiary  w-[300px] p-8 top-12 right-12 absolute z-50">
 
         <div class="sm:mx-auto sm:w-full sm:max-w-sm mb-4 text-gray-200">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 22 22" stroke-width="0.7" stroke="currentColor" class="w-16 h-16 mx-auto">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" class="" />
             </svg>
             <h2 class="text-3xl   font-light">Sign in</h2>
-            <p class="text-xs italic mt-4 w-full text-center">Required fields <span class="text-app-orange">*</span></p>
+            <p class="text-xs italic mt-4 w-full text-left">Required fields <span class="text-app-orange ml-1">*</span></p>
         </div>
 
 
@@ -18,7 +18,7 @@
                 <div class="mt-2">
                     <div class="">
                         <label for="email">Email <span class="text-lg text-red-500">*</span></label>
-                        <input required id="email" name="email" type="email" autocomplete="email" class="block w-full rounded-md border-0 outline-0 py-0.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-500 focus:ring-2 focus:ring-inset focus:ring-gray-700 sm:text-sm sm:leading-6">
+                        <input required id="email" name="email" type="email" autocomplete="email" class="block w-full rounded-md border-0 outline-0 py-0.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-500 focus:ring-2 focus:ring-inset focus:ring-gray-700 sm:text-sm sm:leading-6" placeholder="Email">
                     </div>
                 </div>
 
@@ -26,16 +26,16 @@
                 <div class="mt-1">
                     <div class="">
                     <label for="password">Password <span class="text-lg text-red-500">*</span></label>
-                        <input required  id="password" name="password" type="password" pattern="^(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z])[A-Za-z0-9]{8,}$" title="Minimum 8 characters with at least one number and uppercase."  class="block w-full rounded-md border-0 outline-0 py-0.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-500 focus:ring-2 focus:ring-inset focus:ring-gray-700 sm:text-sm sm:leading-6">
+                        <input required  id="password" name="password" type="password" pattern="^(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z])[A-Za-z0-9]{8,}$" title="Minimum 8 characters with at least one number and uppercase."  class="block w-full rounded-md border-0 outline-0 py-0.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-500 focus:ring-2 focus:ring-inset focus:ring-gray-700 sm:text-sm sm:leading-6" placeholder="Password">
                     </div>
                 </div>
 
                 <!-- Buttons -->
-                <div class="mt-4 bg-app-blue rounded-full">
-                    <button type="submit" name="login" class=" flex w-full justify-center rounded-full  px-3 py-0.5 text-sm font-semibold leading-6 text-app-orange">Sign in</button>
+                <div class="mt-4 bg-app-blue rounded-md">
+                    <button type="submit" name="login" class=" flex w-full justify-center rounded-md  px-3 py-0.5 text-sm font-semibold leading-6 text-app-orange">Sign in</button>
                 </div>
                 <div class="group mt-2">
-                    <div  id="login-cancel" class="flex w-full justify-center rounded-full border border-app-tertiary px-3 py-0.5 text-sm font-semibold leading-6 text-gray-200  cursor-pointer group-hover:bg-app-tertiary">Cancel</div>
+                    <div  id="login-cancel" class="flex w-full justify-center rounded-md border border-app-secondary px-3 py-0.5 text-sm font-semibold leading-6 text-gray-200  cursor-pointer group-hover:bg-app-secondary">Cancel</div>
                 </div>
             </form>
         </div>
