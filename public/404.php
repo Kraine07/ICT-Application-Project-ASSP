6923702aa846017978f0740ee55105466c9d4c02
Orville hall <orvillehall@Orvilles-Mac-mini.local>
2024-02-17 21:34:54 -0500

    added 404 error page

--- public/404.php
+++ public/404.php
@@ -5,9 +5,20 @@ $_SESSION['patron-view'] = true;
 
 require_once('./partials/head.php');
 ?>
-
-
-
+<div class='background-404'>
+   <div class="backdrop">
+   <div>
+        <img src="./img/404-image.png" alt="Oops!">
+    </div>
+    <div class ='text-404'>
+        <h1>PAGE NOT FOUND</h1>
+        <p>The page you are trying to access is unavailable. 
+            We are working hard to resolve this issue in short order. 
+            Please try again later.
+        </p>
+    </div>
+   </div>
+</div>
 <?php
 
 require_once('./partials/footer.php');
